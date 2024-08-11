c6e12088e93bcf33e3fdd0c7ec17a719f7d4bc82
Bielsen <gabriels.v9@gmail.com>
2024-08-11 01:14:21 -0300

    Beggining of Core API webscapring of Mirror Scan

---
+++ src/main/java/tech/bielsen/mirror_scan_api/MirrorScanApiApplication.java
@@ -0,0 +1,13 @@
+package tech.bielsen.mirror_scan_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirrorScanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirrorScanApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/bielsen/mirror_scan_api/MirrorScanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.bielsen.mirror_scan_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirrorScanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
