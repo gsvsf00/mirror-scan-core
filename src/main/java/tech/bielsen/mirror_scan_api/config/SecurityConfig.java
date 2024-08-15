29b2ed7bd4bccccc0ff9c5e842d09c1c5c70ae99
Bielsen <gabriels.v9@gmail.com>
2024-08-15 12:15:02 -0300

    Setting up DB and some security

---
+++ src/main/java/tech/bielsen/mirror_scan_api/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package tech.bielsen.mirror_scan_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.*;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager user(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("gsvsf00")
+                        .password("{noop}password")
+                        .authorities("read")
+                        .build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests( auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.anyRequest().authenticated();
+                })
+                .oauth2Client(withDefaults())
+                .formLogin(withDefaults())
+                .build();
+    }
+
+}
---
+++ src/main/java/tech/bielsen/mirror_scan_api/controller/HomeController.java
@@ -0,0 +1,21 @@
+package tech.bielsen.mirror_scan_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String home(Principal principal) {
+        return "Hello, " + principal.getName() ;
+    }
+
+    @GetMapping("/secured")
+    public String secured() {
+        return "Secured endpoint";
+    }
+
+}
